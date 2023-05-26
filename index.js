cbfb4cb67406179aab91185b8639d0e55e2d1856
maddox earles <mdxear@gmail.com>
2023-05-26 17:11:28 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    return (fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: `${userName}`,
+            email: `${userEmail}`,
+        }),
+    })
+        .then(res => res.json())
+        .then(function (json) {
+            const newId = json.id
+            const newHeader = document.getElementById("h1")
+            newHeader.innerHTML = `${newId}`
+        })
+        .catch(function (error) {
+            const newHeader2 = document.getElementById("h2")
+            newHeader2.innerHTML = `${error}`
+        })
+    )
+}
